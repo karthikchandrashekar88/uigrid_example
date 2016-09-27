9c79f3c662849ff74d0330632f7b9cd752c03850
Manasa S <Manasa.S@lnttechservices.com>
2016-09-27 12:11:18 +0530

    --WIP--

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/anime', function (req, res) {
-  res.sendFile( __dirname + "/public/" + "Anime.html" );
+// app.get('/anime', function (req, res) {
+//   res.sendFile( __dirname + "/public/" + "Anime.html");
+// });
+
+app.get('/animeData1', function (req, res) {
+  console.log("function1 invoked!!!!!");
+   var data1= [{
+       "series": "Cowboy Bebop 1",
+       "character": "Spike",
+       "pic": "http://cdn.paper4pc.com/images/cowboy-bebop-spike-spiegel-wallpaper-2.jpg"
+   }, {
+       "series": "Cowboy Bebop 2",
+       "character": "Spike",
+       "pic": "http://static.rogerebert.com/uploads/movie/movie_poster/dont-breathe-2016/large_dont_breathe.jpg"
+   }, {
+       "series": "Cowboy Bebop 3",
+       "character": "Spike",
+       "pic": "http://cdn.paper4pc.com/images/cowboy-bebop-spike-spiegel-wallpaper-3.jpg"
+   }, {
+       "series": "Cowboy Bebop 4",
+       "character": "Spike",
+       "pic": "http://cdn.paper4pc.com/images/cowboy-bebop-spike-spiegel-wallpaper-1.jpg"
+   }]
+  res.send(data1);
 });
 
+app.get('/animeData2', function (req, res) {
+  console.log("function2 invoked....");
+  var data2 =  [{
+       "series": "Cowboy Bebop 8",
+       "character": "Spike",
+       "pic": "http://static.rogerebert.com/uploads/movie/movie_poster/dont-breathe-2016/large_dont_breathe.jpg"
+   }, {
+       "series": "Cowboy Bebop 9",
+       "character": "Spike",
+       "pic": "http://cdn.paper4pc.com/images/cowboy-bebop-spike-spiegel-wallpaper-2.jpg"
+   }, {
+       "series": "Cowboy Bebop 10",
+       "character": "Spike",
+       "pic": "http://cdn.paper4pc.com/images/cowboy-bebop-spike-spiegel-wallpaper-1.jpg"
+   }, {
+       "series": "Cowboy Bebop 11",
+       "character": "Spike",
+       "pic": "http://cdn.paper4pc.com/images/cowboy-bebop-spike-spiegel-wallpaper-2.jpg"
+   }];
+  res.send(data2);
+});
 
 app.get('/process_get', function (req, res) {
 
